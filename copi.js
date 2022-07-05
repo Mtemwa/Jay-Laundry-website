174f770b4648e0d80e44920be479c7cdcb050482
Mtemwa <84676327+Mtemwa@users.noreply.github.com>
2022-07-05 15:02:22 +0300

    Run index.html file

---
+++ copi.js
@@ -0,0 +1,7 @@
+rnorm(40,0.98,34.5)
+pb = pbinom(40,0.98,34.5)
+rn = rnorm(40,0.98,34.5)
+v = var(rn)
+data.frame(1, row.names = NULL, check.rows = FALSE, check.names = TRUE, stringsAsFactors = default.stringsAsFactors())
+paste0("a", "b")
+paste
